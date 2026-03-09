4b65bf624b335260f5514a9b4425e9b3fca75ce8
IsabelleYuka <isabelle.origassa@escola.pr.gov.br>
2026-03-09 11:13:18 -0300

    Add files via upload

---
+++ aula 1 variaveis.py
@@ -0,0 +1,10 @@
+#Variavel é um espaço de memoria que
+# tem um nome e guarda algum dado do meu programa
+nome = "Jose"
+sobrenome = "Valim"
+numero = 37
+print(nome+sobrenome)
+print(numero)
+
+resultado = numero + numero + 37
+print = (resultado)
